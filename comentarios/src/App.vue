f4b786627c6fb6f01268987ccc7c958cd86a5996
EDUARDO PEREIRA SILVA <goagro71@agrogyn.local>
2021-08-02 17:20:15 -0300

    Aplicação Iniciada atraves do vue CLI

---
+++ comentarios/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <Comment />
+</template>
+
+<script>
+  import Comment from './components/Comment'
+import Comment from './components/Comment.vue'
+  export default{
+  name: 'app',
+  components: {
+    Comment
+    }
+  }
+</script>
+
+<style>
+
+</style>
